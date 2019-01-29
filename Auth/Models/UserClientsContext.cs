0913f6f8c237b0d2b75391f9eb4dd670023e3181
Armen Sarkisyan <a.sarkisian94@gmail.com>
2019-01-29 21:35:46 +0200

    1

---
+++ Auth/Models/UserClientsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Models
+{
+    public class UserClientsContext : DbContext
+    {
+        public DbSet<UserClients> userClients { get; set; }
+
+        public UserClientsContext(DbContextOptions<UserClientsContext> options) :
+            base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
